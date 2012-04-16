a030f9937323a767773e7dc26a4d2d4dd4ce466b
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2012-04-17 00:09:30 +0900

    initial commit

---
+++ lib/Net/TinyIp.pm
@@ -0,0 +1,32 @@
+package Net::TinyIp;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::TinyIp -
+
+=head1 SYNOPSIS
+
+  use Net::TinyIp;
+
+=head1 DESCRIPTION
+
+Net::TinyIp is
+
+=head1 AUTHOR
+
+kuniyoshi E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
