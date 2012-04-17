1c78b7a45b4ef37fbcfb4bab8861afd5eebfb315
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2012-04-17 22:40:17 +0900

    writing constructor

--- lib/Net/TinyIp.pm
+++ lib/Net/TinyIp.pm
@@ -1,22 +1,79 @@
 package Net::TinyIp;
 use strict;
 use warnings;
+use Carp qw( croak );
+use Math::BigInt;
+
 our $VERSION = '0.01';
+
+sub import {
+    my $class = shift;
+    my @tags;
+
+    foreach my $tag ( @_ ) {
+        my $module = join q{::}, $class, map { ucfirst } split m{_}, $tag;
+        eval "require $module"
+            or die;
+    }
+}
+
+sub parse_int {
+    my $class = shift;
+    my $ascii = shift;
+
+    return $class->parse_int_as_v4( $ascii )
+        if $ascii =~ m{ [.] }msx;
+    return $class->parse_int_as_v6( $ascii )
+        if $ascii =~ m{ [:] }msx;
+}
+
+sub parse_int_as_v4 {
+    my $class = shift;
+    my $ascii = shift;
+
+    return Math::BigInt->from_bin(
+        join q{}, q{0b}, map { sprintf "%08b", $_ } split m{[.]}, $ascii,
+    );
+}
+
+sub parse_int_as_v6 {
+    my $class = shift;
+    my $ascii = shift;
+
+    return Math::BigInt->from_hex(
+        join q{}, q{0x}, map { $_ } split m{[:]}, $ascii,
+    );
+}
+
+sub new {
+    my $class = shift;
+    my( $host, $mask ) = @_;
+    my %self;
+
+    croak "Host required"
+        unless defined $host;
+
+    $self{int} = $class->parse_int( $host );
+
+    return bless \%self, $class;
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Net::TinyIp -
+Net::TinyIp - IP object
 
 =head1 SYNOPSIS
 
   use Net::TinyIp;
+  my $ip = Net::TinyIp->new( "192.168.1.1" );
+  say $ip;
 
 =head1 DESCRIPTION
 
-Net::TinyIp is
+Net::TinyIp represents IP address.
 
 =head1 AUTHOR
 
@@ -30,3 +87,4 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
